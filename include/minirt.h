843d2fb4b6a22c95e2f7f341a1213b74b591d98f
yliu <yliu@XLIJFELKNIX.local>
2025-05-05 10:27:29 +0900

    Add minirt.h objects.h vector.h

---
+++ include/minirt.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 23:34:31 by yliu              #+#    #+#             */
+/*   Updated: 2025/05/05 10:26:32 by yliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "objects.h"
+
+typedef double	t_degree;
+
+// ratio : 0.0 - 1.0
+typedef struct s_ambient
+{
+	t_rgb		color;
+	double		ratio;
+}				t_ambient;
+
+// fov : 0.0 - 180.0
+typedef struct s_camera
+{
+	t_rgb		position;
+	t_rgb		orientation;
+	t_degree	fov;
+}				t_camera;
+
+// brightness : 0.0 - 1.0
+typedef struct s_light
+{
+	t_rgb	position;
+	t_rgb	color;
+	double	brightness;
+}			t_light;
+
+typedef struct s_object
+{
+	t_object_type	type;
+	void			*data;
+}					t_object;
+
+typedef struct s_scene
+{
+	t_ambient		ambient;
+	t_camera		camera;
+	t_list			*lights;
+	t_list			*objects;
+}					t_scene;
+
+#endif
