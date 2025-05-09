b739fff27d0f8b1b497227e4c625a8ae269288a0
raks24 <rask0x18@gmail.com>
2025-05-10 01:41:23 +0900

    docs: add viewport documentation

--- include/viewport.h
+++ include/viewport.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 19:58:04 by reasuke           #+#    #+#             */
-/*   Updated: 2025/05/10 01:29:20 by reasuke          ###   ########.fr       */
+/*   Updated: 2025/05/10 01:38:59 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 
 # include "vector.h"
 
+/**
+ * Viewport Configuration for Ray Tracing
+ *
+ * This viewport is configured in a right-handed coordinate system where:
+ * - The camera is positioned on the positive Z-axis looking toward -Z direction
+ * - The viewport plane is parallel to the XY plane
+ * - X-axis corresponds to the horizontal direction (right is positive)
+ * - Y-axis corresponds to the vertical direction (down is positive)
+ *
+ * The viewport is represented by:
+ * - origin: Top-left corner of the viewport
+ * - dx: Unit step vector in the horizontal direction (per pixel)
+ * - dy: Unit step vector in the vertical direction (per pixel)
+ *
+ * These vectors allow for mapping from pixel coordinates (i,j) to
+ * world coordinates using: origin + i*dx + j*dy
+ */
 typedef struct s_viewport
 {
 	t_vec3	origin;
