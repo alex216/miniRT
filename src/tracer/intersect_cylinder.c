6c29d576650ff8b939c6fd95dd6546ee6aa64518
raks24 <rask0x18@gmail.com>
2025-05-18 17:45:33 +0900

    refactor: refactor find_closest_hit

--- src/tracer/intersect_cylinder.c
+++ src/tracer/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:06:27 by reasuke           #+#    #+#             */
-/*   Updated: 2025/05/17 22:52:58 by reasuke          ###   ########.fr       */
+/*   Updated: 2025/05/18 17:44:33 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,19 +40,20 @@ static t_disk	create_disk_from_cylinder(t_cylinder *cy, bool is_top)
 	return (disk);
 }
 
-static bool	find_closest_hit(bool hit_results[], t_hit_record temp_records[],
-				int *min_index, double *min_t)
+static bool	find_closest_hit( bool hit_results[], t_hit_record temp_records[],
+				int count, int *min_index)
 {
 	bool	hit_found;
 	int		i;
+	double	min_t;
 
 	hit_found = false;
 	*min_index = -1;
-	*min_t = INFINITY;
+	min_t = INFINITY;
 	i = 0;
-	while (i < CYLINDER_COMPONENTS)
+	while (i < count)
 	{
-		if (hit_results[i] && ft_fchmin(min_t, temp_records[i].t))
+		if (hit_results[i] && ft_fchmin(&min_t, temp_records[i].t))
 		{
 			*min_index = i;
 			hit_found = true;
@@ -67,7 +68,6 @@ bool	intersect_cylinder(t_ray ray, t_cylinder *cy, t_hit_record *hit_record)
 	t_hit_record	temp_records[CYLINDER_COMPONENTS];
 	bool			hit_results[CYLINDER_COMPONENTS];
 	int				min_index;
-	double			min_t;
 
 	ft_bzero(temp_records, sizeof(temp_records));
 	cy->axis = vec3_normalize(cy->axis);
@@ -76,7 +76,8 @@ bool	intersect_cylinder(t_ray ray, t_cylinder *cy, t_hit_record *hit_record)
 			create_disk_from_cylinder(cy, false), &temp_records[BOTTOM]);
 	hit_results[TOP] = intersect_disk(ray,
 			create_disk_from_cylinder(cy, true), &temp_records[TOP]);
-	if (find_closest_hit(hit_results, temp_records, &min_index, &min_t))
+	if (find_closest_hit(
+			hit_results, temp_records, CYLINDER_COMPONENTS, &min_index))
 	{
 		*hit_record = temp_records[min_index];
 		return (true);
