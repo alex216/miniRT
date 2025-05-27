fea13f5f8e83d19ca0a4eb926fb4d98b953e8b5d
raks24 <rask0x18@gmail.com>
2025-05-27 21:26:49 +0900

    fix: solve_quadratic

--- src/tracer/quadratic.c
+++ src/tracer/quadratic.c
@@ -6,13 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 20:04:19 by reasuke           #+#    #+#             */
-/*   Updated: 2025/05/17 20:32:57 by reasuke          ###   ########.fr       */
+/*   Updated: 2025/05/27 21:24:31 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include <math.h>
 
+#include "libft.h"
 #include "tracer.h"
 
 bool	solve_quadratic(t_quadratic_coef coef, t_quadratic_result *result)
@@ -24,5 +25,7 @@ bool	solve_quadratic(t_quadratic_coef coef, t_quadratic_result *result)
 		return (false);
 	result->s1 = (-coef.b - sqrt(discriminant)) / (2.0 * coef.a);
 	result->s2 = (-coef.b + sqrt(discriminant)) / (2.0 * coef.a);
+	if (result->s1 > result->s2)
+		ft_fswap(&result->s1, &result->s2);
 	return (true);
 }
